612fea4dcd7b4740bbda9e2a339d5bfacdba34d8
David Selassie <selassid@gmail.com>
2014-06-26 13:20:30 -0700

    First working version.

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 housecash
 =========
+Tool for spliting up house bills.
 
-Some tools for spliting up bills.
+Usage
+-----
+Run `python ledger.py HOUSE_YAML BILLS_YAML` to split up a list of bills.
+
+This will figure out who owes what fraction of each bill and output the split. Each bill is split evenly amongs all of the people in the house, based on the fraction of the time of the bill that they were in residence. If person A is in residence the entire duration of a bill, and person B is there only half of the bills time, person A will owe 3/4ths of it and person B 1/4th.
+
+House Definition
+----------------
+A house has a `name`, a `min_people` that it should contain, and a list of `people` who have lived there.
+
+You'll get an error if you try to split up a bill between fewer people than the house is supposed to contain.
+
+### Person
+A person has a `name` and a list of `residencies` which are each a date range. Each residency is a contiguous block of time that they've lived there. If they go away for a summer, they have two residencies: before they left and after they've returned.
+
+### Date Range
+A date range describes a time span. It always has a `start`. It might have an `end`, but could be unbounded into the future. It's also possible, for convenience, to specify the end date as the first day that's _not_ in the range via `end_exclusive`.
+
+It's possible to specify the entire month of January 2014 by either of the following
+```yaml
+start: 2014-01-01
+end: 2014-01-31
+
+start: 2014-01-01
+end_exclusive: 2014-02-01
+```
+
+### Example House
+```yaml
+house:
+  name: Housetub
+  min_people: 3
+  people:
+    - name: David
+      residencies:
+        - start: 2013-09-01
+          end_exclusive: 2014-09-01
+    - name: Stubbs
+      residencies:
+        - start: 2013-08-01
+          end: 2014-05-23
+        - start: 2014-08-06
+```
+
+Bill Definition
+---------------
+The bill YAML is a list of bills. Each bill has a `description`, a `total_cost` in dollars, and `for_dates` which is a single date range.
+
+### Example Bills
+```yaml
+bills:
+  - description: Garbage
+    total_cost: 178.08
+    for_dates:
+      start: 2014-03-31
+      end: 2014-06-14
+```
--- ledger.py
+++ ledger.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3.4
-"""
-"""
+"""USAGE: ledger.py HOUSE_YAML BILLS_YAML
 
+Calculate how much each person in a house owes to split a bill evenly
+amongst all people who are living in a house.
 
+See README.md for YAML definitions.
+"""
 import arrow
 from collections import namedtuple
 import datetime
@@ -10,25 +13,24 @@ import yaml
 import sys
 
 
-PRESENT_DAY = 'NOW'
+FUTURE = arrow.get('9999-01-01')  # This is a date far in the future.
 DateRange = namedtuple('DateRange', (
     'start',
     'end',
 ))
 Bill = namedtuple('Bill', (
-    'type_id',
+    'description',
     'for_dates',
     'total_cost',
-    'payer_id',
 ))
 Person = namedtuple('Person', (
-    'id_name',
-    'residency_dates',
+    'name',
+    'residencies',
 ))
-Payment = namedtuple('Payment', (
-    'payer_id',
-    'payee_id',
-    'amount',
+House = namedtuple('House', (
+    'name',
+    'min_people',
+    'people',
 ))
 
 
@@ -38,7 +40,32 @@ def date_range_length(dr):
     :type dr: DateRange
     :rtype: timedelta
     """
+    if (
+            dr.start == FUTURE
+            or dr.end == FUTURE
+            and not (
+                dr.start == FUTURE
+                and dr.end == FUTURE
+            )
+    ):
+        raise ValueError('no length of an unbounded date range')
     return dr.end - dr.start
+
+
+def date_range_intersection(a, b):
+    """Find the intersection date range of two other date ranges.
+
+    :type a: DateRange
+    :type b: DateRange
+    :rtype: DateRange
+    """
+    i_lb = max(a.start, b.start)
+    i_ub = min(a.end, b.end)
+    if i_ub >= i_lb:
+        return DateRange(start=i_lb, end=i_ub)
+    else:
+        # Return an empty date range.
+        return DateRange(start=i_lb, end=i_lb)
 
 
 def date_range_overlap_fraction(a, b):
@@ -48,22 +75,97 @@ def date_range_overlap_fraction(a, b):
     :type b: DateRange
     :rtype: float
     """
-    i_lb = max(a.start, b.start)
-    i_ub = min(a.end, b.end)
-    i_range = i_lb - i_ub
-    if i_range < timedelta():
-        return 0.0
-    else:
-        return i_range / date_range_length(a)
+    i = date_range_intersection(a, b)
+    return date_range_length(i) / date_range_length(a)
 
 
-def intersect(bill, person):
-    return date_range_overlap_fraction(bill.for_dates, person.residency_dates) * person.weight * bill.total_cost
+def calc_relative_responsibility(bill_date_range, person):
+    """Figure out the _relative_ share of a bill a person is responsible
+    for.
+
+    If a person is only in residence for half of the bill's duration,
+    they are responsible for 0.5 a person's share. Someone who is in
+    residence for the entirety of a bill is responsible for 1.0 of a
+    share.
+
+    :type bill_date_range: DateRange
+    :type person: Person
+    :rtype: float
+    """
+    return sum(
+        date_range_overlap_fraction(bill_date_range, residency)
+        for residency
+        in person.residencies
+    )
+
+
+def calc_all_relative_responsibilities(bill, people):
+    """Calculate the relative responsabilities of a list of people for a
+    bill.
+
+    :type bill: Bill
+    :type people: iterable of Persons
+    :rtype: dict from Person to float
+    """
+    return {
+        person: calc_relative_responsibility(bill.for_dates, person)
+        for person
+        in people
+    }
+
+
+def calc_personal_costs_from_responsibilities(bill_total_cost, person_to_relative_responsibility):
+    """Given relative responsibilities for all people, determine what part
+    of the total cost of a bill each owes.
+
+    :type bill: Bill
+    :type person_to_relative_responsibility: dict from Person to float
+    :rtype: dict from Person to float in dollars
+    """
+    total_responsibility = sum(person_to_relative_responsibility.values())
+    return {
+        person: relative_responsibility / total_responsibility * bill_total_cost
+        for person, relative_responsibility
+        in person_to_relative_responsibility.items()
+    }
+
+
+def calc_all_personal_costs(bill, house):
+    """Calculate what dollar amounts of a bill are owed by all people in the
+    house.
+
+    :type bill: Bill
+    :type house: House
+    :rtype: dict from Person to float in dollars
+    """
+    person_to_relative_responsibility = calc_all_relative_responsibilities(
+        bill,
+        house.people,
+    )
+
+    if sum(person_to_relative_responsibility.values()) < house.min_people:
+        raise ValueError('house is under-rented during {0}'.format(
+            bill.for_dates,
+        ))
+
+    person_to_cost = calc_personal_costs_from_responsibilities(
+        bill.total_cost,
+        person_to_relative_responsibility,
+    )
+
+    total_personal_costs = sum(person_to_cost.values())
+    if round(bill.total_cost, 2) != round(total_personal_costs, 2):
+        raise RuntimeError('bill split failed: total is {0}; split total is {1}'.format(
+            bill.total_cost,
+            total_personal_costs,
+        ))
+
+    return person_to_cost
 
 
 def type_date(d):
-    if d == PRESENT_DAY:
-        return arrow.utcnow()
+    if isinstance(d, arrow.arrow.Arrow):
+        return d
     elif isinstance(d, datetime.date):
         return arrow.get(datetime.datetime.combine(d, datetime.time()), 'UTC')
     else:
@@ -71,43 +173,71 @@ def type_date(d):
 
 
 def type_date_range(d):
-    return DateRange(start=type_date(d['start']), end=type_date(d['end']))
+    start_date = type_date(d['start'])
+    if 'end' not in d and 'end_exclusive' not in d:
+        end_date = FUTURE
+    if 'end' in d:
+        end_date = type_date(d['end'])
+    elif 'end_exclusive' in d:
+        end_date = type_date(d['end_exclusive']) - datetime.timedelta(days=1)
+    return DateRange(start=start_date, end=end_date)
 
 
 def type_person(d):
     return Person(
-        id_name=d['id_name'],
-        residency_dates=type_date_range(d['residency_dates']),
+        name=d['name'],
+        residencies=tuple(type_date_range(r) for r in d['residencies']),
     )
 
 
 def type_bill(d):
     return Bill(
-        type_id=d['type_id'],
+        description=d['description'],
         for_dates=type_date_range(d['for_dates']),
         total_cost=float(d['total_cost']),
-        payer_id=d['payer_id'],
     )
 
 
-def type_payment(d):
-    return Payment(
-        payer_id=d['payer_id'],
-        payee_id=d['payee_id'],
-        amount=float(d['amount']),
+def type_house(d):
+    return House(
+        name=d['name'],
+        min_people=int(d['min_people']),
+        people=tuple(type_person(p) for p in d['people']),
     )
 
 
-def load_yaml(f):
-    with open(f, 'r') as fp:
-        d = yaml.safe_load(fp)
+def load_yaml(fn):
+    with open(fn, 'r') as fp:
+        return yaml.safe_load(fp)
 
-    bills = tuple(type_bill(bd) for bd in d['bills'])
-    people = tuple(type_person(pd) for pd in d['people'])
-    payments = tuple(type_payment(pd) for pd in d['payments'])
 
-    return bills, people, payments
+def main(house_fn, bills_fn):
+    house = type_house(load_yaml(house_fn)['house'])
+    bills = tuple(type_bill(b) for b in load_yaml(bills_fn)['bills'])
+
+    for bill in bills:
+        person_to_cost = calc_all_personal_costs(bill, house)
+        print('----')
+        print_bill(bill)
+        print_person_to_cost(person_to_cost)
+
+
+def print_bill(bill):
+    print('For {0!r} from {1} to {2} totalling ${3:.2f}:'.format(
+        bill.description,
+        bill.for_dates.start.date(),
+        bill.for_dates.end.date(),
+        bill.total_cost,
+    ))
+
+
+def print_person_to_cost(person_to_cost):
+    print('Costs:')
+    for person, cost in sorted(person_to_cost.items()):
+        if cost > 0.0:
+            print('  {0}: ${1:.2f}'.format(person.name, cost))
 
 
 if __name__ == '__main__':
-    print(load_yaml(sys.argv[1]))
+    house_fn, bills_fn = sys.argv[1:]
+    main(house_fn, bills_fn)
